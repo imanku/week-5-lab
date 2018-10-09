18d30d6e92d8d5e6075e8223141f66c0aa2f6d22
imanku <43012896+imanku@users.noreply.github.com>
2018-10-09 09:36:52 +0530

    commit again

--- week-5-lab.cpp
+++ week-5-lab.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 #include <math.h>
+#include <fstream>
 
 int main()
 {
  using std::cout;
  using std::endl;
  using std::cin;
- int a,b,c;
+ int a,b;
  cout<<"Enter any two integers"<<endl;
  cout<<"Enter the value of a= "<<endl;
  cin>>a;
  cout<<"Enter the value of b= "<<endl;
  cin>>b;
  
- c=pow(a,2)+pow(b,2);
- auto d=sqrt(c);
+ auto  c = sqrt(pow(a,2) + pow(b,2));
 
- cout<<"The third side = "<<d<<endl;
+ cout<<"The third side = "<<c<<endl;
+ std::ofstream myfile;
+ myfile.open(" example.txt ");
+ if (myfile<<" your triangle has three sides width "<<a<<" , height "<<b<<", breadth are "<<c);
 
+ myfile.close();
+ 
  return 0;
 }
